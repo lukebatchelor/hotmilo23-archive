e587ce2002dbba27d8b51cd2c14f1c572775a75d
Luke.Batchelor <batchelor.luke+git@gmail.com>
2021-07-10 21:19:32 +1000

    Creates basic site with link to first page, /upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        This website is an archive of some of the static content that lived at{' '}
+        <a href="https://hotmilo23.com">hotmilo23.com</a> for several years. That site was decimissioned in 2021 as it
+        was hosted on an extremely expensive managed hosting service and was running a lot of dodgy php with trivial RCE
+        bugs present.
+      </p>
+      <p>
+        For the nonstatic sites I've either removed them or tried to present them in a read-only way that matches what
+        the old experience would have been
+      </p>
+      <p>
+        Presented in no particular order:
+        <ul>
+          <li>
+            <a href="/public_html/upload/index.htm">public_html/upload/index.htm</a> - a (now non-functioning) file
+            upload portal. What a fantastic Idea that was... Along with an empty <code>/a</code> directory and a
+            javascript file that was definitely designed for a different game? (<code>/otter.js</code>)
+          </li>
+        </ul>{' '}
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
     </div>
   );
 }
---
+++ www/public_html/upload/upload.php
@@ -0,0 +1,30 @@
+<?php
+	$target_dir = "a/";
+	$target_file = $target_dir . basename($_FILES["uploadFile"]["name"]);
+	$uploadOk = 1;
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+
+	}
+	// Check if file already exists
+	if (file_exists($target_file)) {
+	    echo "Sorry, file already exists. $target_file";
+	    $uploadOk = 0;
+	}
+	// Check file size
+	if ($_FILES["fileToUpload"]["size"] > 500000) {
+	    echo "Sorry, your file is too large.";
+	    $uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+	    echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+	    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+	        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	    } else {
+	        echo "Sorry, there was an error uploading your file. $target_file";
+	    }
+	}
+?>
